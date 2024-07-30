2b996f3f7fd7aa63fea52084c907b2c70fff3f45
unknown <22211tt1473@mail.tdc.edu.vn>
2024-07-30 09:10:07 +0700

    a

--- models/payment_transaction.py
+++ models/payment_transaction.py
@@ -18,26 +18,40 @@ class PaymentTransaction(models.Model):
     _inherit = "payment.transaction"
 
     order_url = fields.Char(string="Order URL")
-    _logger.info("Dữ liệu phản hồi từ ZaloPayyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy")
-    # def _handle_zalopay_response(self, data):
-    #     """
-    #     Xử lý phản hồi từ ZaloPay.
-    #     """
-    #     # Ghi log dữ liệu phản hồi để kiểm tra
-    #     _logger.info("Dữ liệu phản hồi từ ZaloPay: %s", data)
 
-    #     # Lấy order_url từ dữ liệu phản hồi
-    #     order_url = data.get('order_url')
+    def _handle_zalopay_response(self, data):
+        """
+        Xử lý phản hồi từ ZaloPay.
+        """
+        _logger.info("Dữ liệu phản hồi từ ZaloPay: %s", data)
 
-    #     # Ghi log order_url để kiểm tra
-    #     if order_url:
-    #         _logger.info("Order URL từ phản hồi ZaloPay: %s", order_url)
-    #     else:
-    #         _logger.info("Không có Order URL trong phản hồi ZaloPay")
+        # Lấy order_url từ dữ liệu phản hồi
+        order_url = data.get('order_url')
 
-    #     # Tìm hoặc tạo giao dịch thanh toán
-    #     tx = self.search([('reference', '=', data.get('reference'))])
-    #     if tx:
-    #         tx.write({'order_url': order_url})
+        # Ghi log order_url để kiểm tra
+        if order_url:
+            _logger.info("Order URL từ phản hồi ZaloPay: %s", order_url)
+        else:
+            _logger.info("Không có Order URL trong phản hồi ZaloPay")
+
+        # Tìm hoặc tạo giao dịch thanh toán
+        tx = self.search([('reference', '=', data.get('reference'))])
+        if tx:
+            _logger.info("Giao dịch thanh toán tìm thấy với reference: %s", data.get('reference'))
+            tx.write({'order_url': order_url})
+        else:
+            _logger.info("Không tìm thấy giao dịch thanh toán với reference: %s", data.get('reference'))
+
+        return tx
+
+    def _process_payment_response(self, data):
+        """
+        Xử lý phản hồi thanh toán.
+        """
+        _logger.info("Bắt đầu xử lý phản hồi thanh toán: %s", data)
         
-    #     return tx
+        # Xử lý phản hồi từ ZaloPay
+        self._handle_zalopay_response(data)
+        
+        # Các bước xử lý khác nếu cần
+

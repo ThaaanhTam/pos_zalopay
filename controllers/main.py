e1739afa12aab8c62c069343af8d3dff828c38be
unknown <22211tt1473@mail.tdc.edu.vn>
2024-08-01 23:45:24 +0700

    a

--- controllers/main.py
+++ controllers/main.py
@@ -2,6 +2,7 @@ import hmac
 import hashlib
 import logging
 import requests as pyreq
+
 from odoo.http import request
 import json
 import random
@@ -9,7 +10,14 @@ from io import BytesIO
 from datetime import datetime
 import base64
 import qrcode
-from odoo import http
+from werkzeug.urls import url_encode
+from werkzeug.exceptions import Forbidden
+
+from odoo import http, _, tools
+from odoo.exceptions import AccessError, ValidationError, UserError
+from odoo.addons.payment.controllers.post_processing import PaymentPostProcessing
+from odoo.addons.payment.controllers import portal as payment_portal
+from odoo.http import request
 
 _logger = logging.getLogger(__name__)
 
@@ -130,107 +138,107 @@ class ZaloPayController(http.Controller):
 
 
 
-        @http.route(_callback_url, type='http', auth='public', methods=['POST'], csrf=False)
-        def zalopay_callback(self, **post):
-            """Handle the callback request from ZaloPay."""
-            _logger.info("Received ZaloPay callback")
+    @http.route(_callback_url, type='http', auth='public', methods=['POST'], csrf=False)
+    def zalopay_callback(self, **post):
+        """Handle the callback request from ZaloPay."""
+        _logger.info(" ZaloPay callbackkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk")
 
-            # Get the data from the request
-            data = request.get_json_data()
-            _logger.info("Callback data: %s", data)
+        # Get the data from the request
+        data = request.get_json_data()
+        _logger.info("Callback data: %s", data)
 
-            try:
-                # Get the POS order with the app_trans_id
-                pos_order_sudo = (
-                    request.env["pos.order"]
-                    .sudo()
-                    .search([("id", "=", data.get("app_trans_id"))], limit=1)
-                )
+        try:
+            # Get the POS order with the app_trans_id
+            pos_order_sudo = (
+                request.env["pos.order"]
+                .sudo()
+                .search([("id", "=", data.get("app_trans_id"))], limit=1)
+            )
 
-                # Check if the order exists
-                if not pos_order_sudo:
-                    raise ValidationError(_("Không tìm thấy giao dịch phù hợp với mã tham chiếu."))
+            # Check if the order exists
+            if not pos_order_sudo:
+                raise ValidationError(_("Không tìm thấy giao dịch phù hợp với mã tham chiếu."))
 
-                # Check if the order has been paid
-                if pos_order_sudo.state in ("paid", "done", "invoiced"):
-                    _logger.info("Đơn hàng đã được thanh toán. Đang hủy bỏ.")
-                    return json.dumps({"return_code": 2, "return_message": "Đơn hàng đã được thanh toán."})
+            # Check if the order has been paid
+            if pos_order_sudo.state in ("paid", "done", "invoiced"):
+                _logger.info("Đơn hàng đã được thanh toán. Đang hủy bỏ.")
+                return json.dumps({"return_code": 2, "return_message": "Đơn hàng đã được thanh toán."})
 
-                # Get the ZaloPay provider
-                zalopay = (
-                    request.env["payment.provider"]
-                    .sudo()
-                    .search([("code", "=", "zalopay")], limit=1)
-                )
+            # Get the ZaloPay provider
+            zalopay = (
+                request.env["payment.provider"]
+                .sudo()
+                .search([("code", "=", "zalopay")], limit=1)
+            )
 
-                # Verify the callback data
-                data_string = f"{data['app_id']}|{data['app_trans_id']}|{data['app_user']}|{data['amount']}|{data['app_time']}|{data['embed_data']}|{data['item']}"
-                mac = hmac.new(zalopay.key2.encode(), data_string.encode(), hashlib.sha256).hexdigest()
+            # Verify the callback data
+            data_string = f"{data['app_id']}|{data['app_trans_id']}|{data['app_user']}|{data['amount']}|{data['app_time']}|{data['embed_data']}|{data['item']}"
+            mac = hmac.new(zalopay.key2.encode(), data_string.encode(), hashlib.sha256).hexdigest()
 
-                if mac != data['mac']:
-                    raise Forbidden(_("Nhận dữ liệu với chữ ký không hợp lệ."))
+            if mac != data['mac']:
+                raise Forbidden(_("Nhận dữ liệu với chữ ký không hợp lệ."))
 
-                # Validate the amount
-                order_amount = pos_order_sudo._get_checked_next_online_payment_amount()
-                receive_amount = data.get("amount")
-                if int(receive_amount) != int(order_amount):
-                    raise AssertionError(_("Số tiền không khớp."))
+            # Validate the amount
+            order_amount = pos_order_sudo._get_checked_next_online_payment_amount()
+            receive_amount = data.get("amount")
+            if int(receive_amount) != int(order_amount):
+                raise AssertionError(_("Số tiền không khớp."))
 
-                # Create a new transaction
-                tx_sudo = self.create_new_transaction(pos_order_sudo, zalopay, order_amount)
+            # Create a new transaction
+            tx_sudo = self.create_new_transaction(pos_order_sudo, zalopay, order_amount)
 
-                # Update the transaction "provider_reference" with the zp_trans_id
-                tx_sudo.provider_reference = data.get("zp_trans_id")
+            # Update the transaction "provider_reference" with the zp_trans_id
+            tx_sudo.provider_reference = data.get("zp_trans_id")
 
-                # Check the response code and process the payment
-                if data.get("status") == 1:  # Assuming 1 means successful payment
-                    _logger.info("Thanh toán được xử lý thành công. Đang lưu.")
+            # Check the response code and process the payment
+            if data.get("status") == 1:  # Assuming 1 means successful payment
+                _logger.info("Thanh toán được xử lý thành công. Đang lưu.")
 
-                    # Set the transaction as done and process the payment
-                    tx_sudo._set_done()
-                    tx_sudo._process_pos_online_payment()
-                    _logger.info("Thanh toán đã được lưu thành công.")
-                    return json.dumps({"return_code": 1, "return_message": "Đặt hàng thành công."})
-                else:
-                    _logger.warning(
-                        "Nhận dữ liệu với mã phản hồi không hợp lệ: %s. Đặt trạng thái giao dịch thanh toán thành Lỗi.",
-                        data.get("status"),
-                    )
-                    tx_sudo._set_error(
-                        "ZaloPay: "
-                        + _("Nhận dữ liệu với mã phản hồi không hợp lệ: %s", data.get("status"))
-                    )
-                    return json.dumps({"return_code": 2, "return_message": f"Nhận dữ liệu với mã lỗi là: {data.get('status')}"})
-
-            except Forbidden:
+                # Set the transaction as done and process the payment
+                tx_sudo._set_done()
+                tx_sudo._process_pos_online_payment()
+                _logger.info("Thanh toán đã được lưu thành công.")
+                return json.dumps({"return_code": 1, "return_message": "Đặt hàng thành công."})
+            else:
                 _logger.warning(
-                    "Lỗi cấm trong quá trình xử lý thông báo. Đang hủy bỏ.",
-                    exc_info=True,
+                    "Nhận dữ liệu với mã phản hồi không hợp lệ: %s. Đặt trạng thái giao dịch thanh toán thành Lỗi.",
+                    data.get("status"),
                 )
-                return json.dumps({"return_code": 2, "return_message": "Sai thông tin xác thực."})
-
-            except AssertionError:
-                _logger.warning(
-                    "Lỗi khẳng định trong quá trình xử lý thông báo. Đang hủy bỏ.",
-                    exc_info=True,
+                tx_sudo._set_error(
+                    "ZaloPay: "
+                    + _("Nhận dữ liệu với mã phản hồi không hợp lệ: %s", data.get("status"))
                 )
-                return json.dumps({"return_code": 2, "return_message": "Số tiền không chính xác.", "data": {"amount": f"{int(order_amount)}"}})
+                return json.dumps({"return_code": 2, "return_message": f"Nhận dữ liệu với mã lỗi là: {data.get('status')}"})
 
-            except ValidationError:
-                _logger.warning(
-                    "Lỗi xác thực trong quá trình xử lý thông báo. Đang hủy bỏ.",
-                    exc_info=True,
-                )
-                return json.dumps({"return_code": 2, "return_message": "Không tìm thấy Mã đơn hàng trong hệ thống.", "data": {"app_trans_id": data.get("app_trans_id")}})
+        except Forbidden:
+            _logger.warning(
+                "Lỗi cấm trong quá trình xử lý thông báo. Đang hủy bỏ.",
+                exc_info=True,
+            )
+            return json.dumps({"return_code": 2, "return_message": "Sai thông tin xác thực."})
 
-            except Exception as e:
-                _logger.error(f"Lỗi xử lý dữ liệu callback: {e}")
-                return json.dumps({"return_code": 2, "return_message": f"Lỗi hệ thống khi xử lý thông tin: {e}"})
-                
+        except AssertionError:
+            _logger.warning(
+                "Lỗi khẳng định trong quá trình xử lý thông báo. Đang hủy bỏ.",
+                exc_info=True,
+            )
+            return json.dumps({"return_code": 2, "return_message": "Số tiền không chính xác.", "data": {"amount": f"{int(order_amount)}"}})
+
+        except ValidationError:
+            _logger.warning(
+                "Lỗi xác thực trong quá trình xử lý thông báo. Đang hủy bỏ.",
+                exc_info=True,
+            )
+            return json.dumps({"return_code": 2, "return_message": "Không tìm thấy Mã đơn hàng trong hệ thống.", "data": {"app_trans_id": data.get("app_trans_id")}})
+
+        except Exception as e:
+            _logger.error(f"Lỗi xử lý dữ liệu callback: {e}")
+            return json.dumps({"return_code": 2, "return_message": f"Lỗi hệ thống khi xử lý thông tin: {e}"})
+            
 
 
 
-    
-    
+
+
 
 

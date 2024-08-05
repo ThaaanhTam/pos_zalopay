68b3ba2c012d042720e5244409980095a48680f9
unknown <22211tt1473@mail.tdc.edu.vn>
2024-08-05 09:25:04 +0700

    a

--- models/pos_order.py
+++ models/pos_order.py
@@ -4,3 +4,5 @@ class PosOrder(models.Model):
     _inherit = 'pos.order'
 
     app_trans_id = fields.Char(string="Mã Giao Dịch ZaloPay")
+    zalopay_result = fields.Char("ZaloPay Result")
+    zalopay_status = fields.Char("ZaloPay Status")

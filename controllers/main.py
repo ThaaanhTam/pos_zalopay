46609e18396f86f093a93538546b1da4e69f1667
unknown <22211tt1473@mail.tdc.edu.vn>
2024-08-05 08:31:01 +0700

    a

--- controllers/main.py
+++ controllers/main.py
@@ -236,7 +236,7 @@ class ZaloPayController(http.Controller):
             result['return_message'] = str(e)
         _logger.info("Kết thúc xử lý callback ZaloPay với kết quả: %s", result)
         # Thông báo kết quả cho ZaloPay server
-        return result
+        # return result
 
 
 
